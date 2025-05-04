63a7137c41a17819c7863cbe1db30a9938eaeb96
HenriqueGiberti <henriquegiberti@gmail.com>
2025-05-04 09:07:26 -0300

    tome

--- TP2/Entidades/aed3/ArqAtor.java
+++ TP2/Entidades/aed3/ArqAtor.java
@@ -42,7 +42,7 @@ public class ArqAtor extends Arquivo<Ator> {
             return null;
     }
     
-    public Ator readId(int pid){
+    public Ator readId(int pid) throws Exception{
       return read(pid);
     }
 
